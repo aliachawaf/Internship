7ca2c40dc1eb6ff05c75fca3532a933bddc595b5
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-06-29 14:48:53 +0100

    FEAT read logfile and record fields

--- Logfiles/src/aliachawaf/Logfile.java
+++ Logfiles/src/aliachawaf/Logfile.java
@@ -1,5 +1,10 @@
 package aliachawaf;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Logfile {
 
 	private String fileName;
@@ -8,6 +13,7 @@ public class Logfile {
 	// constructor
 	public Logfile(String fileName) {
 		this.fileName = fileName;
+		this.setFields();
 	}
 
 	// getters & setters
@@ -19,13 +25,22 @@ public class Logfile {
 		return fields;
 	}
 	
-	public String setFields() {
-		
-		
-		
+	public void setFields() {
+		try {
+			File file = new File(fileName);
+
+			BufferedReader b = new BufferedReader(new FileReader(file));
+
+			String line = "";
+
+			while ((line = b.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			b.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
-	// methods
-	
-
 }
