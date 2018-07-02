b6fa1e5d42f6645b79adcddac2c5fb7e92a42c1e
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-07-02 17:52:35 +0100

    FEAT main function

--- Logfiles/src/aliachawaf/AnalyseLogfile.java
+++ Logfiles/src/aliachawaf/AnalyseLogfile.java
@@ -1,18 +1,41 @@
 package aliachawaf;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class AnalyseLogfile {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Enter the name of your logfile (path) : ");
-		
-		String fileName = scanner.nextLine();
-		
-		
-		
+		ListRegexp listRegexp = new ListRegexp();
+		listRegexp.setListRegexp("expandedREDefs.txt");
+
+		ListLogPatterns listLogPatterns = new ListLogPatterns();
+		listLogPatterns.setListPatterns("LogPatterns.txt");
+
+		boolean fileNotFound = true;
+		String fileName;
+
+		System.out.print("Enter the name of your logfile (path) : ");
+
+		do {
+
+			fileName = scanner.nextLine();
+			Logfile logFile = new Logfile(fileName);
+			logFile.setFields();
+
+			File file = new File(fileName);
+
+			if (Files.isReadable(file.toPath())) {
+
+				System.out.println("\n" + logFile.compareAllLogPatterns(listLogPatterns, listRegexp));
+				fileNotFound = false;
+			}
+		} while (fileNotFound);
+
+		scanner.close();
 	}
 }
