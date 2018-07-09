8d3f261c079dd1d226a5cbdd05d608507269bace
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-07-09 15:07:28 +0100

    FEAT add startline and finishline

--- Logfiles/src/aliachawaf/AnalyseLogfile.java
+++ Logfiles/src/aliachawaf/AnalyseLogfile.java
@@ -31,8 +31,8 @@ public class AnalyseLogfile {
 
 			if (Files.isReadable(file.toPath())) {
 
-				//System.out.println("\n" + logFile.compareAllLogPatterns(listLogPatterns, listRegexp));
-				System.out.print("header line : " + logFile.hasHeaderLine(listLogPatterns, listRegexp));
+				System.out.println("\n" + logFile.compareAllLogPatterns(listLogPatterns, listRegexp, 0, 20));
+				//System.out.print("header line : " + logFile.hasHeaderLine(listLogPatterns, listRegexp));
 				fileNotFound = false;
 			}
 		} while (fileNotFound);
--- Logfiles/src/aliachawaf/Logfile.java
+++ Logfiles/src/aliachawaf/Logfile.java
@@ -34,19 +34,19 @@ public class Logfile {
 
 	// read the logfile and add its lines in the list
 	public void setFields() {
-		
-		try {		
+
+		try {
 			Reader reader = Files.newBufferedReader(Paths.get(fileName));
 			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-			
+
 			for (CSVRecord line : csvParser) {
-				
+
 				this.listLines.add(line);
 				
 			}
 
 			csvParser.close();
-			
+
 		} catch (java.nio.file.NoSuchFileException e) {
 			System.out.print("File not found ! Please check your input (path) and re-enter it : ");
 		} catch (IOException e) {
@@ -54,59 +54,201 @@ public class Logfile {
 		}
 	}
 
-	
 	// compare each lines of the logfile with the pattern entered as parameter
-	public int compareLogPattern(LogPattern pattern, ListRegexp listRegexp) {
+//	public int compareLogPattern(LogPattern pattern, ListRegexp listRegexp) {
+//
+//		int nbLinesMatching = 0;
+//		boolean matches = true;
+//
+//		String regexNameExpected;
+//		String regexDefExpected;
+//
+//		// we analyse each line of logfile
+//		for (CSVRecord line : this.listLines) {
+//
+//			if (line.size() == pattern.getListRegexName().size()) {
+//
+//				// for each field of the current line, we check if it matches the regex expected
+//				for (int i = 0; i < line.size(); i++) {
+//
+//					// when a field doesn't matches, we don't analyse the next fields
+//					if (matches) {
+//
+//						regexNameExpected = pattern.getListRegexName().get(i);
+//
+//						// we get the definition of the regex from its name
+//						regexDefExpected = listRegexp.getDefinitionByName(regexNameExpected);
+//
+//						// compare the current field of the line with the pattern's regex expected
+//						matches = Pattern.matches(regexDefExpected, line.get(i));
+//					}
+//				}
+//
+//				if (matches) {
+//					nbLinesMatching++;
+//				} else {
+//					// for the next line to analyse, we give matches its initial value
+//					matches = true;
+//				}
+//
+//			} else {
+//				// if the line doesn't have the same number of fields of the pattern, then it
+//				// doesn't match
+//				matches = false;
+//			}
+//		}
+//		return nbLinesMatching;
+//	}
+//
+//	// compare all the lines of the logfile with all the patterns of listLogPatterns
+//	public String compareAllLogPatterns(ListLogPatterns listLogPatterns, ListRegexp listRegexp) {
+//
+//		String result = "";
+//		String patternMatching = "Pattern(s) matching : ";
+//		int nbLinesMatching;
+//
+//		for (LogPattern pattern : listLogPatterns.getListPatterns()) {
+//
+//			nbLinesMatching = this.compareLogPattern(pattern, listRegexp);
+//
+//			result = result + pattern.getLogInfos()[0] + " pattern" + pattern.getLogInfos()[2] + " : " + nbLinesMatching
+//					+ " / " + this.getListLines().size() + "\n";
+//
+//			if (nbLinesMatching == this.getListLines().size()) {
+//				patternMatching = patternMatching + pattern.getLogInfos()[0] + " n°" + pattern.getLogInfos()[2] + " ; ";
+//			}
+//		}
+//
+//		result = patternMatching + "\n\n" + result;
+//
+//		return result;
+//	}
+
+	public boolean hasHeaderLine(ListLogPatterns listLogPatterns, ListRegexp listRegexp) {
+		// returns true if the first line is header line, else returns false
+
+		boolean matches = true;
+		int nbPatternMatching = 0;
+
+		String regexNameExpected;
+		String regexDefExpected;
+
+		CSVRecord firstLine = this.listLines.get(0);
+
+		// for each pattern of the list
+		for (LogPattern pattern : listLogPatterns.getListPatterns()) {
+
+			if (firstLine.size() == pattern.getListRegexName().size()) {
+				// for each field of the line
+				for (int i = 0; i < firstLine.size(); i++) {
+
+					if (matches && i < pattern.getListRegexName().size()) {
+
+						// we get the regex we expect to match with from the current pattern
+						regexNameExpected = pattern.getListRegexName().get(i);
+
+						// we get the definition of the regex from its name
+						regexDefExpected = listRegexp.getDefinitionByName(regexNameExpected);
+
+						// compare the current field of the line with the pattern's regex expected
+						matches = Pattern.matches(regexDefExpected, firstLine.get(i));
+					}
+				}
+
+				if (matches) {
+					nbPatternMatching++;
+				} else {
+					matches = true;
+				}
+
+			} else {
+				matches = false;
+			}
+		}
+
+		System.out.println(nbPatternMatching);
+		// if none pattern matches the first line, then it is a header line
+		return (nbPatternMatching == 0);
+	}
+
+	// compare each lines of the logfile with the pattern entered as parameter
+	public int compareLogPattern(LogPattern pattern, ListRegexp listRegexp, int startLine, int finishLine) {
 
 		int nbLinesMatching = 0;
 		boolean matches = true;
 
 		String regexNameExpected;
 		String regexDefExpected;
+		
+		// we consider that when startLine and finishLine are both equal to -1, then we have to analyse ALL the lines of the file
+		if (startLine==-1 && finishLine==-1) {
+			finishLine = this.listLines.size();
+		}
 
 		// we analyse each line of logfile
-		for (CSVRecord line : this.listLines) {
+		for (int l = startLine; l <= finishLine; l++) {
 
-			// for each field of the current line, we check if it matches the regex expected
-			for (int i = 0; i < line.size(); i++) {
-				
-				if (matches && i < pattern.getListRegexName().size()) {
+			CSVRecord line = this.listLines.get(l);
+			// for (CSVRecord line : this.listLines) {
 
-					regexNameExpected = pattern.getListRegexName().get(i);
+			if (line.size() == pattern.getListRegexName().size()) {
 
-					// we get the definition of the regex from its name
-					regexDefExpected = listRegexp.getDefinitionByName(regexNameExpected);
+				// for each field of the current line, we check if it matches the regex expected
+				for (int i = 0; i < line.size(); i++) {
 
-					// compare the current field of the line with the pattern's regex expected
-					matches = Pattern.matches(regexDefExpected, line.get(i));
+					// when a field doesn't matches, we don't analyse the next fields
+					if (matches) {
+
+						regexNameExpected = pattern.getListRegexName().get(i);
+
+						// we get the definition of the regex from its name
+						regexDefExpected = listRegexp.getDefinitionByName(regexNameExpected);
+
+						// compare the current field of the line with the pattern's regex expected
+						matches = Pattern.matches(regexDefExpected, line.get(i));
+					}
 				}
-			}
 
-			if (matches) {
-				nbLinesMatching++;
+				if (matches) {
+					nbLinesMatching++;
+				} else {
+					// for the next line to analyse, we give matches its initial value
+					matches = true;
+				}
+
 			} else {
-				matches = true;
+				// if the line doesn't have the same number of fields of the pattern, then it
+				// doesn't match
+				matches = false;
 			}
 		}
 		return nbLinesMatching;
 	}
 
-	
 	// compare all the lines of the logfile with all the patterns of listLogPatterns
-	public String compareAllLogPatterns(ListLogPatterns listLogPatterns, ListRegexp listRegexp) {
+	public String compareAllLogPatterns(ListLogPatterns listLogPatterns, ListRegexp listRegexp, int startLine, int finishLine) {
 
 		String result = "";
 		String patternMatching = "Pattern(s) matching : ";
 		int nbLinesMatching;
+		
+		int nbLinesProcessed;
+		
+		// we consider that when startLine and finishLine are both equal to -1, then we have to analyse ALL the lines of the file
+		if (startLine==-1 && finishLine==-1) {
+			nbLinesProcessed = this.listLines.size();
+		} else {
+			nbLinesProcessed = finishLine-startLine+1;
+		}
 
 		for (LogPattern pattern : listLogPatterns.getListPatterns()) {
 
-			nbLinesMatching = this.compareLogPattern(pattern, listRegexp);
+			nbLinesMatching = this.compareLogPattern(pattern, listRegexp, startLine, finishLine);
 
 			result = result + pattern.getLogInfos()[0] + " pattern" + pattern.getLogInfos()[2] + " : " + nbLinesMatching
-					+ " / " + this.getListLines().size() + "\n";
+					+ " / " + nbLinesProcessed + "\n";
 
-			if (nbLinesMatching == this.getListLines().size()) {
+			if (nbLinesMatching == nbLinesProcessed) {
 				patternMatching = patternMatching + pattern.getLogInfos()[0] + " n°" + pattern.getLogInfos()[2] + " ; ";
 			}
 		}
@@ -114,37 +256,5 @@ public class Logfile {
 		result = patternMatching + "\n\n" + result;
 
 		return result;
-	}
-	
-	
-	public boolean hasHeaderLine(ListLogPatterns listLogPatterns, ListRegexp listRegexp) {
-		
-		boolean headerLine = false;
-		String regexNameExpected;
-		String regexDefExpected;
-		
-		CSVRecord firstLine = this.listLines.get(0);
-		
-		for (LogPattern pattern : listLogPatterns.getListPatterns()) {
-			
-		
-			for (int i = 0; i < firstLine.size(); i++) {
-
-				if (!headerLine) {
-				regexNameExpected = pattern.getListRegexName().get(i);
-
-				// we get the definition of the regex from its name
-				regexDefExpected = listRegexp.getDefinitionByName(regexNameExpected);
-
-				// compare the current field of the line with the pattern's regex expected
-				headerLine = Pattern.matches(regexDefExpected, firstLine.get(i));
-				System.out.println(headerLine);
-				}
-			}
-
-		}
-		
-		return !headerLine;
-		
 	}
 }
