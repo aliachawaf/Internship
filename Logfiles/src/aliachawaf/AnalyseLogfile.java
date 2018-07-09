674bcf96ccac33ddfac35152d6291a28be8c14ad
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-07-09 15:31:20 +0100

    FEAT input lines

--- Logfiles/src/aliachawaf/AnalyseLogfile.java
+++ Logfiles/src/aliachawaf/AnalyseLogfile.java
@@ -17,27 +17,51 @@ public class AnalyseLogfile {
 		listLogPatterns.setListPatterns("LogPatterns.txt");
 
 		boolean fileNotFound = true;
+		boolean linesInput = false;
 		String fileName;
+		int startLine = 0;
+		int finishLine = 0;
+		Logfile logFile;
 
 		System.out.print("Enter the name of your logfile (path) : ");
 
 		do {
 
 			fileName = scanner.nextLine();
-			Logfile logFile = new Logfile(fileName);
+			logFile = new Logfile(fileName);
 			logFile.setFields();
 
 			File file = new File(fileName);
 
+			
 			if (Files.isReadable(file.toPath())) {
-
-				System.out.println("\n" + logFile.compareAllLogPatterns(listLogPatterns, listRegexp, 0, 20));
-				//System.out.print("header line : " + logFile.hasHeaderLine(listLogPatterns, listRegexp));
+				
 				fileNotFound = false;
 			}
 		} while (fileNotFound);
 
 		
+		
+		System.out.print("Do you want to analyse all the lines (yes/no)? ");
+		
+		do {
+			String allLines = scanner.nextLine();
+			
+			if (allLines.toLowerCase().matches("yes")) {
+				startLine = -1;
+				finishLine = -1;
+				linesInput = true;
+			} else if (allLines.toLowerCase().matches("no")) {
+				System.out.println("enter lines : ");
+				startLine = scanner.nextInt();
+				finishLine = scanner.nextInt();
+				linesInput = true;
+			}
+			
+		} while (!linesInput);
+		
+		System.out.println("\n" + logFile.compareAllLogPatterns(listLogPatterns, listRegexp, startLine, finishLine));
+		//System.out.print("header line : " + logFile.hasHeaderLine(listLogPatterns, listRegexp));
 		
 		scanner.close();
 	}
