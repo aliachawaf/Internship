2beb4060f192c0d7ebe1193f6c8398d60edd3602
aliachawaf <alia.chawaf@etu.umontpellier.fr>
2018-07-19 16:11:16 +0100

    PERF work with not expanded file

--- LogFile/src/aliachawaf/ListRegexp.java
+++ LogFile/src/aliachawaf/ListRegexp.java
@@ -21,10 +21,9 @@ public class ListRegexp {
 		return listRegexp;
 	}
 
-	/* setter : 
-	 * we read the file of regular expressions entered in parameter, 
-	 * collect the regex found (name + def), 
-	 * and then form the list of regex
+	/*
+	 * setter : we read the file of regular expressions entered in parameter,
+	 * collect the regex found (name + def), and then form the list of regex
 	 */
 	public void setListRegexp(String fileName) {
 
@@ -38,19 +37,23 @@ public class ListRegexp {
 
 			// while we haven't reached the end of file
 			while ((line = b.readLine()) != null) {
-				
-				// we split the line in 2 to have the name and the def of the regex
-				regexp = line.split(" ", 2);
 
-				/* regexp[0] is the name
-				 * regexp[1] is the definition 
-				 */
-				
-				// create the regex found with the couple name/def
-				Regexp re = new Regexp(regexp[0], regexp[1]);
+				// analyse the line only if it is not empty and not a comment
+				if (!line.isEmpty() && !line.startsWith("#")) {
+					// we split the line in 2 to have the name and the def of the regex
+					// (split depending on a tab or a space)
+					regexp = line.split("\t| ", 2);
 
-				// add the regex to the list
-				this.listRegexp.add(re);
+					/* regexp[0] is the name 
+					 * regexp[1] is the definition
+					 */
+											
+					// create the regex found with the couple name/def
+					Regexp re = new Regexp(regexp[0], regexp[1]);
+
+					// add the regex to the list
+					this.listRegexp.add(re);
+				}
 			}
 
 			b.close();
@@ -60,10 +63,12 @@ public class ListRegexp {
 		}
 	}
 
+	
 	// methods
 	public String getDefinitionByName(String name) {
-	
-		// read the list of regex and returns the def corresponding to the name in parameter		
+
+		// read the list of regex and returns the def corresponding to the name in
+		// parameter
 		String def = null;
 
 		for (Regexp re : this.listRegexp) {
@@ -73,6 +78,33 @@ public class ListRegexp {
 			}
 		}
 		return def;
+	}
+	
+	// replace all %{NAME} by their regex definition	
+	public void replaceNamesByDef() {
+		
+		String defToReplace, nameToBeReplaced, name; 
+		int indexEndName;
+		
+		for (Regexp re : this.listRegexp) {
+			
+			while (re.getDefinition().indexOf("%{")!=-1) {
+				
+				indexEndName = re.getDefinition().indexOf("%{")+1;
+				
+				while (re.getDefinition().charAt(indexEndName)!='}') {
+					indexEndName++;
+				}
+				
+				nameToBeReplaced = re.getDefinition().substring(re.getDefinition().indexOf("%{"), indexEndName+1);
+
+				name = nameToBeReplaced.substring(2, nameToBeReplaced.length()-1);
+				
+				defToReplace = this.getDefinitionByName(name.split(":")[0]);
+
+				re.setDefinition(re.getDefinition().replace(nameToBeReplaced, defToReplace));
+			}	
+		}	
 	}
 
 	@Override
--- LogFile/src/aliachawaf/Regexp.java
+++ LogFile/src/aliachawaf/Regexp.java
@@ -19,6 +19,10 @@ public class Regexp {
 		return definition;
 	}
 
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+
 	@Override
 	public String toString() {
 		return name + " " + definition;
